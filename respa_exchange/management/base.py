c3fb7bbc549175f090dfca015d7e8ababe8183de
Aarni Koskela <aarni.koskela@andersinno.fi>
2016-08-09 08:25:28 +0300

    Refactor management command

---
+++ respa_exchange/management/base.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+
+from respa_exchange.models import ExchangeResource
+
+rx_logger = logging.getLogger("respa_exchange")
+
+
+def get_active_download_resources(exchange_configs):
+    """
+    Get resources that are enabled for downward (to-Respa) sync.
+
+    :param exchange_configs: Exchange configurations to look at. These will be assigned to the respective resources.
+    :type exchange_configs: list[respa_exchange.models.ExchangeConfiguration]
+    :rtype: list[respa_exchange.models.ExchangeResource]
+    """
+    resources = []
+    for exchange in exchange_configs:
+        for ex_resource in ExchangeResource.objects.filter(
+            sync_to_respa=True,
+            exchange=exchange,
+            exchange__enabled=True
+        ):
+            ex_resource.exchange = exchange  # Allow sharing the EWS session
+            resources.append(ex_resource)
+    return resources
+
+
+def configure_console_log():
+    rx_logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(logging.Formatter(
+        fmt="%(asctime)s - %(name)s - %(levelname)s: %(message)s",
+        datefmt=logging.Formatter.default_time_format
+    ))
+    rx_logger.addHandler(handler)
